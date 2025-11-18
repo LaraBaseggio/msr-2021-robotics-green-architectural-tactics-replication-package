c3845215e363ea34e2b98e8140ecf84610647fc2
LaraBaseggio <l.baseggio@student.vu.nl>
2025-11-18 18:10:09 +0000

    ran repo_to_csv

--- RQ1_data_software/phase1_data_collection/data_to_csv/repo_to_csv.py
+++ RQ1_data_software/phase1_data_collection/data_to_csv/repo_to_csv.py
@@ -41,11 +41,48 @@ while '' in columns['URL']:
     columns['URL'].remove('')
 
 with open('../git_scraper/data2/git_repos1_data.json') as f:
-    repo_data = json.load(f)
+    # Handle JSONL format (one JSON object per line) or multiple JSON objects
+    repo_data = []
+    content = f.read()
+    
+    # First, try to parse as a single JSON array/object
+    try:
+        repo_data = json.loads(content)
+        if not isinstance(repo_data, list):
+            repo_data = [repo_data]
+    except json.JSONDecodeError:
+        # If that fails, try JSONL format (one JSON object per line)
+        repo_data = []
+        for line in content.split('\n'):
+            line = line.strip()
+            if line:
+                try:
+                    repo_data.append(json.loads(line))
+                except json.JSONDecodeError:
+                    pass
+        
+        # If JSONL didn't work, try to extract concatenated JSON objects
+        if not repo_data:
+            decoder = json.JSONDecoder()
+            idx = 0
+            content_remaining = content
+            while idx < len(content_remaining):
+                # Skip whitespace and commas (JSON objects may be separated by commas)
+                while idx < len(content_remaining) and (content_remaining[idx].isspace() or content_remaining[idx] == ','):
+                    idx += 1
+                if idx >= len(content_remaining):
+                    break
+                try:
+                    obj, end_idx = decoder.raw_decode(content_remaining[idx:])
+                    repo_data.append(obj)
+                    idx += end_idx
+                except (json.JSONDecodeError, ValueError):
+                    break
 
 repo_url = []
 repo_name = [item.get('git_repo_name') for item in repo_data]
 repo_name_new = []
+repo_indices = []  # Track which original repo index each matched entry corresponds to
 repo_md_contents = [item.get('md_contents') for item in repo_data]
 repo_md_file = [item.get('md_file_names') for item in repo_data]
 repo_code_c = [item.get('code_comments_c++') for item in repo_data]
@@ -60,12 +97,13 @@ new_repo_url1 = []
 new_repo_name = []
 new_repo_name1 = []
 
-for name in repo_name:
+for i, name in enumerate(repo_name):
     for url in columns['URL']:
         if url.endswith(name):
             print(name+": ",url)
             repo_url.append(url)
             repo_name_new.append(name)
+            repo_indices.append(i)  # Track the original repository index
 print(len(repo_url))
 print(len(repo_name_new))
 print(len(repo_name))
@@ -77,17 +115,30 @@ d =  Counter(repo_name)
 res = [k for k, v in d.items() if v > 1]
 #print(res)
 
+def flatten_and_join(value):
+    """Flatten nested lists and join into a single string."""
+    if isinstance(value, list):
+        # Flatten nested lists
+        flattened = []
+        for item in value:
+            if isinstance(item, list):
+                flattened.extend(item)
+            else:
+                flattened.append(item)
+        return ''.join(str(x) for x in flattened)
+    return str(value) if value else ''
+
 for mcontents in repo_md_contents:
     for key in mcontents.keys():
-        mcontents[key] = ''.join(mcontents[key])
+        mcontents[key] = flatten_and_join(mcontents[key])
 
 for cpp in repo_code_c:
     for key in cpp.keys():
-        cpp[key] = ''.join(cpp[key])
+        cpp[key] = flatten_and_join(cpp[key])
 
 for py in repo_code_p:
     for key in py.keys():
-        py[key] = ''.join(py[key])
+        py[key] = flatten_and_join(py[key])
 
 # # print(len(repo_md_contents))
 # # print(len(repo_code_c))
@@ -122,21 +173,19 @@ for m in repo_md_contents:
 #print(new_dicts_md)
 
 
-i = 0
-for url in repo_url:
-    longest_list = return_longest_list(repo_code_p[i], repo_md_contents[i], repo_code_c[i])
-    new_repo_url.append([url] * len(repo_code_p[i]))
-    i = i + 1
+for i, url in enumerate(repo_url):
+    repo_idx = repo_indices[i]  # Get the original repository index
+    longest_list = return_longest_list(repo_code_p[repo_idx], repo_md_contents[repo_idx], repo_code_c[repo_idx])
+    new_repo_url.append([url] * len(repo_code_p[repo_idx]))
 
 for url_list in new_repo_url:
     for url in url_list:
         new_repo_url1.append(url)
 
-i = 0
-for name in repo_name:
-    longest_list = return_longest_list(repo_code_p[i], repo_md_contents[i], repo_code_c[i])
-    new_repo_name.append([name] * len(repo_code_p[i]))
-    i = i + 1
+for i, name in enumerate(repo_name):
+    repo_idx = repo_indices[i]  # Get the original repository index
+    longest_list = return_longest_list(repo_code_p[repo_idx], repo_md_contents[repo_idx], repo_code_c[repo_idx])
+    new_repo_name.append([name] * len(repo_code_p[repo_idx]))
 
 for name_list in new_repo_name:
     for name in name_list:
